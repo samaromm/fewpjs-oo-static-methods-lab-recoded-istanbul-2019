8a4354cb4ecab420c8021ecad7e871475e77627d
Samar Omar <samaromm98@gmail.com>
2020-02-14 14:41:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ class Formatter {
   static titleize(str){
     let strArr= str.split(' ')
     let noC=['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    strArr[0]=Formatter.capitalize(strArr[0])
+    
     for(let i=1;i<strArr.length;i++){
       if(!noC.includes(strArr[i])) {strArr[i]=Formatter.capitalize(strArr[i])}
     }
